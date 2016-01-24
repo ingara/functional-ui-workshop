2e65786897099cac047fac79fcbb2dc4a9680efd
Guro <guro.seternes@bekk.no>
2016-01-24 14:48:45 +0100

    Changed content in windows and changed the design to reflect the season

--- src/reducers/windows.js
+++ src/reducers/windows.js
@@ -1,20 +1,18 @@
 import { AppActions } from '../actions';
 
 const defaultState = [
-  { day: 9, content: 'Faggruppeavslutning!', type: 'text', opened: false },
-  { day: 10, content: 'https://i.imgur.com/QAyXWuC.gif', type: 'image', opened: false },
-  { day: 11, content: 'Bekk Julebord', type: 'text', opened: false },
-  { day: 12, content: '8f3RkpTnS7I', type: 'youtube', opened: false },
-  { day: 13, content: '2jlMpvYvjr0', type: 'youtube', opened: false },
-  { day: 14, content: 'http://www.tu.no/incoming/2013/12/17/1200023726.jpg/alternates/h1080/1200023726.jpg', type: 'image', opened: false },
-  { day: 15, content: 'iMblk3L8nOQ', type: 'youtube', opened: false },
-  { day: 16, content: 'spotify:track:2FRnf9qhLbvw8fu4IBXx78', type: 'spotify', opened: false },
-  { day: 17, content: 'I dag skal du ut og kose deg med faggruppa', opened: false },
-  { day: 18, content: 'spotify:track:7KT0MWuO7tQAdhqDXVjaHG', type: 'spotify', opened: false },
-  { day: 19, content: 'Bare fem dager til jul!!', type: 'text', opened: false },
-  { day: 20, content: 'http://matblogg.trinesmatblogg.netdna-cdn.com/wp-content/uploads/2013/12/IMG_2843.jpg', type: 'image', opened: false },
-  { day: 21, content: 'J1Z8SQ1OUa0', type: 'youtube', opened: false },
-  { day: 22, content: 'spotify:track:5YhmqI7XxMTqagUIsCTO95', type: 'spotify', opened: false }
+  { day: 1, content: 'Yo!', type: 'text', opened: false },
+  { day: 2, content: '2jlMpvYvjr0', type: 'youtube', opened: false },
+  { day: 3, content: 'Du er flink', type: 'text', opened: false },
+  { day: 4, content: 'http://tummax.com/wp-content/uploads/2015/05/wow.png', type: 'image', opened: false },
+  { day: 5, content: 'http://deaddarlings.com/wp-content/uploads/2015/01/POW-Weddings.jpg', type: 'image', opened: false },
+  { day: 6, content: 'J1Z8SQ1OUa0', type: 'youtube', opened: false },
+  { day: 7, content: 'spotify:track:1sdjNMLfOmKh3XICWEHh62', type: 'spotify', opened: false },
+  { day: 8, content: 'Redux <3', type: 'text', opened: false },
+  { day: 9, content: '8f3RkpTnS7I', type: 'youtube', opened: false },
+  { day: 10, content: 'iMblk3L8nOQ', type: 'youtube', opened: false },
+  { day: 11, content: 'spotify:track:7KT0MWuO7tQAdhqDXVjaHG', type: 'spotify', opened: false },
+  { day: 12, content: 'spotify:track:5YhmqI7XxMTqagUIsCTO95', type: 'spotify', opened: false }
 ];
 
 export default function(state = defaultState, action = {}) {
